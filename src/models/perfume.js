902c6659c10a638f34ea0e7321ce9510e7ebb124
Saurabh Chamola <saurabh@pearlorganisation.com>
2024-06-24 13:43:40 +0530

    changes in auth sectoon

--- src/models/perfume.js
+++ src/models/perfume.js
@@ -17,9 +17,17 @@ const perfumeSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    gallery: [String],
+
     mainAccords: [{ name: String, color: String, percentage: Number }],
     pyramid: {
-      type: [{ topNote: [String], middleNote: [String], baseNote: [String] }],
+      type: [
+        {
+          topNote: [{ name: String, photo: String }],
+          middleNote: [{ name: String, photo: String }],
+          baseNote: [{ name: String, photo: String }],
+        },
+      ],
     },
   },
   { timestamps: true }
--- src/routes/perfume.js
+++ src/routes/perfume.js
@@ -3,5 +3,5 @@ import { upload } from "../config/cloudinary.js";
 import { getAllPerfume, newPerfume } from "../controllers/perfume.js";
 const router = express.Router();
 
-router.route("/").post(upload.single("banner"), newPerfume).get(getAllPerfume);
+router.route("/").post(upload.fields([]), newPerfume).get(getAllPerfume);
 export default router;

4b2c764b07e25149e7f7dd5906cae12b39923f54
Pearl Organisation™ <52455912+pearlorganisation@users.noreply.github.com>
2024-08-27 16:55:52 +0530

    Update auth.js

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,8 +1,9 @@
 import express from "express";
 import { upload } from "../config/cloudinary.js";
-import { signin, signup } from "../controllers/auth.js";
+import { signin, signup,getAllUsers  } from "../controllers/auth.js";
 const router = express.Router();
 
 router.route("/signin").post(signin);
 router.route("/signup").post(signup);
+router.route("/").get(getAllUsers );
 export default router;

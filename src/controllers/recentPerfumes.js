5dae764585d6a74497a71345c2498988295b8846
jai <jai@pearlorganisation.com>
2024-09-03 19:12:02 +0530

    updates

--- src/controllers/recentPerfumes.js
+++ src/controllers/recentPerfumes.js
@@ -2,14 +2,22 @@ import perfume from "../models/perfume.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 export const getAllRecentPerfume = asyncHandler(async (req, res, next) => {
-    const perfumeData = await perfume
+  const perfumeData = await perfume
     .find()
-    .lean().select("perfume brand banner").sort({createdAt:-1}).limit(25);
-  
-    res.status(200).json({ status: true,message:"Recent Perfume Fetched Successfully !!", data: perfumeData });
-  });
+    .populate({
+      path: "brand",
+      select: "brand",
+    })
+    .lean()
+    .select("perfume banner")
+    .sort({ createdAt: -1 })
+    .limit(25);
 
-
-
-
-
+  res
+    .status(200)
+    .json({
+      status: true,
+      message: "Recent Perfume Fetched Successfully !!",
+      data: perfumeData,
+    });
+});

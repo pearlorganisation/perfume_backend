b397e6889a6e46ab2ef7a7ac0be269df6e781cf6
shashank <shashanknegi@pearlorganisation.com>
2024-09-03 16:35:22 +0530

    ss

--- src/controllers/recentPerfumes.js
+++ src/controllers/recentPerfumes.js
@@ -2,10 +2,9 @@ import perfume from "../models/perfume.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 export const getAllRecentPerfume = asyncHandler(async (req, res, next) => {
-    const {id} = req.params; 
     const perfumeData = await perfume
-    .find({brand:id})
-    .lean().select("perfume brand banner").sort({createdAt:-1}).limit(50);
+    .find()
+    .lean().select("perfume brand banner").sort({createdAt:-1}).limit(25);
   
     res.status(200).json({ status: true,message:"Recent Perfume Fetched Successfully !!", data: perfumeData });
   });
@@ -14,4 +13,3 @@ export const getAllRecentPerfume = asyncHandler(async (req, res, next) => {
 
 
 
-  

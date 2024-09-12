8efd71d03f88b5873e8c98c0864c7e6a2b6c853a
Abhishek Bahuguna <abhishek@pearlorganisation.com>
2024-09-12 18:22:16 +0530

    fixed update celebrity perfume route error

--- src/routes/celebrityPerfumes.js
+++ src/routes/celebrityPerfumes.js
@@ -1,19 +1,24 @@
 import express from "express";
 import { upload } from "../config/cloudinary.js";
-import { addCelebrityPerfume, deleteCelebrityPerfume, getCelebrityPerfume, getCelebrityPerfumes, updateCelebrityPerfume } from "../controllers/celebrityPerfumes.js";
-
+import {
+  addCelebrityPerfume,
+  deleteCelebrityPerfume,
+  getCelebrityPerfume,
+  getCelebrityPerfumes,
+  updateCelebrityPerfume,
+} from "../controllers/celebrityPerfumes.js";
 
 const router = express.Router();
 
 router
   .route("/")
-  .post(
-    upload.fields([{ name: "banner" }]),
-    addCelebrityPerfume
-  )
-  .get(getCelebrityPerfumes)
-  .patch(upload.fields([{ name: "banner" }]) , updateCelebrityPerfume);
+  .post(upload.fields([{ name: "banner" }]), addCelebrityPerfume)
+  .get(getCelebrityPerfumes);
 
-router.route("/:id").delete(deleteCelebrityPerfume).get(getCelebrityPerfume);
+router
+  .route("/:id")
+  .delete(deleteCelebrityPerfume)
+  .get(getCelebrityPerfume)
+  .patch(upload.fields([{ name: "banner" }]), updateCelebrityPerfume);
 
 export default router;

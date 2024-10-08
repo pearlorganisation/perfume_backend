172641d07fad5b84c2edd480ddfaab57e837fc3e
shashank <shashanknegi@pearlorganisation.com>
2024-10-08 16:18:50 +0530

    added changes on to the top rated perfume section

--- src/models/topRatedPerfume.js
+++ src/models/topRatedPerfume.js
@@ -2,16 +2,12 @@ import mongoose from "mongoose";
 
 
 const topRatedSchema = new mongoose.Schema({
-    perfumeName:{
-        type:String
-    },
+
     perfumeId:{
         type:mongoose.Schema.ObjectId,
         ref:"perfume"
     },
-    banner:{
-        type:String
-    }
+
 },{timestamps:true});
 
 

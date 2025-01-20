c74fb6d7e490fb35ed0cad819e43128dc1c80aa3
shashank <shashanknegi@pearlorganisation.com>
2025-01-20 16:46:00 +0530

    added changes

--- src/routes/genderPerfumes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getFemalePerfumes, getMalePerfumes } from "../controllers/perfume.js";
-
-const genderPerfumesRouter = express.Router();
-genderPerfumesRouter.route("/male").get(getMalePerfumes);
-genderPerfumesRouter.route("/female").get(getFemalePerfumes);
-
-export default genderPerfumesRouter;
